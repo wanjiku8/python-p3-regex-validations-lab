27e5b7fca42d7623d65855a673b35d85e9673a24
wanjiku8 <wanjikuf141@gmail.com>
2025-02-21 11:00:26 -0800

    commit

--- lib/regex.py
+++ lib/regex.py
@@ -1,13 +1,10 @@
 import re
 
-# NOTE: There are only a few tests included, so multiple solutions will work.
-# Feel free to encourage students to find oversights and add tests to this lab!
-
-name = r""
+name = r"^[A-Z][a-zA-Z]*(?:[ '-][A-Z][a-zA-Z]*)*$"
 name_regex = re.compile(name)
 
-phone_number = r""
+phone_number = r"^\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}$"
 phone_regex = re.compile(phone_number)
 
-email_address = r""
+email_address = r"^[a-zA-Z][a-zA-Z0-9._%+-]*@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
 email_regex = re.compile(email_address)
